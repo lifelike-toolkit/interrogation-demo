c70a80879af7b69eab64c9056573233a58a36f54
Khoa Nguyen <dangkhoa81122@gmail.com>
2023-05-17 15:00:23 -0400

    Extra stuff

--- setup/build_sequence_tree.py
+++ setup/build_sequence_tree.py
@@ -27,9 +27,10 @@ llm_embedding.client.verbose = False
 # Certain topics have the strongest effects to weakening Jason, but the evidence to contradict those only come in late game.
 tree = KnowledgeTree.from_texts(
     "dinnercase", 
-    ["Emily William, the victim in this case, is an 18 year-old female. She was poisoned to death at the dinner party, which was witnessed by only Peter and Jason. She died right before Peter William made an announcement. She is Jason William's sister, as well as Peter and Catherine's daughter.",
+    ["Jason William is being questioned about the death of Emily William",
+    "Emily William, the victim in this case, is an 18 year-old female. She was poisoned to death at the dinner party, which was witnessed by only Peter and Jason. She died right before Peter William made an announcement. She is Jason William's sister, as well as Peter and Catherine's daughter.",
     "Peter William is Jason William and Emily William's father. He was present at the dinner party. He was the one who called the police after the murder. He witnessed Emily's murder. He kept the identity of the heir to his inheritance secret to everyone but Harvey Specter",
-    "Jason William's family includes Peter William, Catherine William and Emily William, but Tina Bridges also lives with them",
+    "Jason William's family includes Peter William, Catherine William and Emily William, but Tina Bridges also lives with them and works as the family maid",
     "Harvey Specter is Peter William's lawyer. He is extrememly good at his job. He told Jason William that he was the heir to Peter William's inheritance a month ago",
     "Catherine William is Jason William and Emily William's mother, as well as Peter William's wife. She did not witness Emily's death as she was in a different room.",
     "The dinner party happened on Thursday. It was hosted the William family mansion by Peter William himself", 
@@ -39,7 +40,7 @@ tree = KnowledgeTree.from_texts(
 )
 
 # Defines the retriever from tree
-retriever = tree.get_retriever(search_type='similarity', search_kwargs={'k': 3}) # hopefully will reduce the number of returned results
+retriever = tree.get_retriever(search_type='similarity', search_kwargs={'k': 4}) # hopefully will reduce the number of returned results
 
 # Defines the QA chain
 retrievalQA = RetrievalQA.from_llm(llm=llm, retriever=retriever)
